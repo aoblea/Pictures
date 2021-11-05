994f2363c9a2ec3ccef8ff16ee9ad0894abe8c22
aoblea <arwinoblea@gmail.com>
2021-11-04 23:08:22 -0400

    Initial commit

---
+++ Pictures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pictures
+//
+//  Created by Arwin Oblea on 11/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
